10d47cfac57b78532269f872638322e793485ee4
diyalovescuppayo <hiredesai3595@mydusd.org>
2025-05-27 10:25:27 -0700

    Update finalprpject.py

--- finalprpject.py
+++ finalprpject.py
@@ -1,3 +1,8 @@
 def greet(geet):
     """This function greets the person passed in a parameter.""
-    print(f"Hello, {hname}!")
+    print(f"Hello, {name}!")
+def add(x, y):
+        """This function adds two numbers and returns the result."""
+        return x + y
+result = add(5, 3) 
+print(result) # Output: 8
