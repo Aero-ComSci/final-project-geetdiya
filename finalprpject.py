5865e62180a072610e62623a02820b0d2327c5f8
geetmm <malgewar0597@mydusd.org>
2025-05-29 09:19:19 -0700

    Update finalprpject.py

--- finalprpject.py
+++ finalprpject.py
@@ -1,8 +1,33 @@
-def greet(geet):
-    """This function greets the person passed in a parameter.""
-    print(f"Hello, {name}!")
-def add(x, y):
-        """This function adds two numbers and returns the result."""
-        return x + y  
-result = add(5, 3) 
-print(result) # Output: 8
+import random 
+
+strategy name = "Beat GeetnDiya" 
+
+def move (my_history, their_history):
+        pick = random.choice (["r", "p", "s"])
+        if pick == "r":
+            print("rock")
+        if pick == "p":
+            print("paper")
+        if pick == "s":
+            print("scissors")
+        return pick
+
+players = ["Player 1", "Player 2", "Player 3"]
+moves= []
+
+for names in players 
+    print(player + "is playing:")
+    m = move()
+    moves.append(m)
+
+print("n/Moves:")
+print("Player 1:", moves = [0])
+print("Player 2:", moves = [1])
+print (Player 3:", moves = [2])
+
+if moves[0] == moves[1] == moves[2]:
+    print("n/Result: Everyone Wins! Its a tie")
+elif "r" in moves and "p" in moves and "s" in moves 
+    print("n/Results: Its a tie! Everyone wins!")
+else: 
+print(n/Results: Two players picked the same choice, its a tie!")
